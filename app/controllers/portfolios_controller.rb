44fb3a4f5f2218c2b520ab83bbd5c263f9a3dfb7
gitbnw <byron.weiss@gmail.com>
2016-01-23 18:58:43 +0000

    fixing portfolio page to add new stocks

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,14 +6,26 @@ class PortfoliosController < ApplicationController
 
   end
   
-  
-  
 
   def create
     
-    @portfolio.user = current_user
+    @portfolio = Wiki.new
+    @portfolio.name = params[:portfolio][:name]
+    @portfolio.user_id = current_user.id
+    
+     if @portfolio.save
+       flash[:notice] = "Portfolio was saved."
+       redirect_to @portfolio
+     else
 
-
+       flash.now[:alert] = "There was an error saving the Portfolio. Please try again."
+       render :new
+     end
+     
   end
+  
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end  
    
 end
