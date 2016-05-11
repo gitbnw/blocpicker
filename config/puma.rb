2c3d6405dd1221343c2b2cfd6c9e66eb57646d56
gitbnw <byron.weiss@gmail.com>
2016-05-11 20:41:03 +0000

    blah blah deploy

--- config/puma.rb
+++ config/puma.rb
@@ -5,21 +5,21 @@ workers 1
 threads 1, 6
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+shared_dir = "#{app_dir}/shared/tmp"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix:///home/bnw/apps/blocpicker/shared/sockets/puma.sock"
+bind "unix://#{shared_dir}/sockets/puma.sock"
 
 # Logging
-stdout_redirect "/home/bnw/apps/blocpicker/shared/log/puma.stdout.log", "/home/bnw/apps/blocpicker/shared/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "/home/bnw/apps/blocpicker/shared/pids/puma.pid"
-state_path "/home/bnw/apps/blocpicker/shared//pids/puma.state"
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
