d6823ca67c0cc2782f38fd21f06d454ed73e20d3
gitbnw <byron.weiss@gmail.com>
2016-05-12 15:07:33 +0000

    who knows anymore

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-unless Rails.env.production?  
+
 
   # Change to match your CPU core count
   workers 1
@@ -6,28 +6,16 @@ unless Rails.env.production?
   # Min and Max threads per worker
   threads 1, 6
   
-  app_dir = File.expand_path("../..", __FILE__)
-  shared_dir = "#{app_dir}/shared/tmp"
-  
   # Default to production
   rails_env = ENV['RAILS_ENV']
   environment rails_env
   
-  # Set up socket location
-  bind "unix://#{shared_dir}/sockets/blocpicker-puma.sock"
-  
   # Logging
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
   
-  # Set master PID and state locations
-  pidfile "#{shared_dir}/pids/puma.pid"
-  state_path "#{shared_dir}/pids/puma.state"
-  activate_control_app
   
   on_worker_boot do
     require "active_record"
     ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
     ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
   end
-  
-end
