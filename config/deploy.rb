565a2c7439b1871cc743633395293a334c6e7771
gitbnw <byron.weiss@gmail.com>
2016-05-13 14:50:27 +0000

    Add redis recipe to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,20 @@ namespace :puma do
   before :start, :make_dirs
 end
 
+namespace :redis do
+
+    desc "Flushes all Redis data"
+    task :flushall, :roles => [:app] do
+      run "redis-cli flushall"
+    end
+
+    desc "Restart Redis"
+    task :restart, :roles => [:app], :max_hosts => 1 do
+      sudo "/etc/init.d/redis-server stop; sudo /etc/init.d/redis-server start"
+    end
+
+end
+
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
