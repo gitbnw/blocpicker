0082f5d45c34292aef414bc7527abef4c814204d
gitbnw <byron.weiss@gmail.com>
2016-05-13 14:55:40 +0000

    Add redis recipe to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,13 +49,17 @@ end
 namespace :redis do
 
     desc "Flushes all Redis data"
-    task :flushall, :roles => [:app] do
-      run "redis-cli flushall"
+    task :flushall do
+      on roles(:app) do
+        run "redis-cli flushall"
+      end
     end
 
     desc "Restart Redis"
-    task :restart, :roles => [:app], :max_hosts => 1 do
-      sudo "/etc/init.d/redis-server stop; sudo /etc/init.d/redis-server start"
+    task :restart, :max_hosts => 1 do
+      on roles(:app) do
+        sudo "/etc/init.d/redis-server stop; sudo /etc/init.d/redis-server start"
+      end
     end
 
 end
