59501e163e7898d04f6f326dc2fba41d99b453c0
gitbnw <byron.weiss@gmail.com>
2016-05-17 19:53:31 +0000

    deploy changes for resque tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -111,7 +111,7 @@ namespace :deploy do
   end
   
   before :starting,     :check_revision
-  # after :publishing,    :export
+  after :publishing,    :export
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
